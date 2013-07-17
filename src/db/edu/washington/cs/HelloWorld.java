9b666cdb0cca73ed8feafc5edbba0247533a7455
Andrew Whitaker <whitaker@cs.washington.edu>
2013-07-17 07:33:36 -0700

    initial commit

---
+++ src/db/edu/washington/cs/HelloWorld.java
@@ -0,0 +1,12 @@
+package db.edu.washington.cs;
+
+public final class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
