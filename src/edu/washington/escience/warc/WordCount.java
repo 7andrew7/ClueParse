eceb50f283aa6f5c651c5e1463f58bef961cd38e
Andrew Whitaker <whitaker@cs.washington.edu>
2013-07-18 16:31:41 -0700

    switch to new school mapreduce API

--- src/edu/washington/escience/warc/WordCount.java
+++ src/edu/washington/escience/warc/WordCount.java
@@ -7,43 +7,46 @@ import java.util.StringTokenizer;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 public class WordCount extends Configured implements Tool {
 
-	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
 
 		private static final IntWritable one = new IntWritable(1);
 		private Text word = new Text();
 		
 		@Override
-		public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> out, Reporter reporter)
-				throws IOException {
+		public void map(LongWritable key, Text value, Context context) 
+				throws IOException, InterruptedException {
 			String line = value.toString();
 			StringTokenizer tokenizer = new StringTokenizer(line);
 			while (tokenizer.hasMoreTokens()) {
 				word.set(tokenizer.nextToken());
-				out.collect(word, one);
-				reporter.incrCounter("wordcount", "map", 1);
+				context.write(word, one);
 			}
 		}
 	}
 	
-	public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+	public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
 
 		@Override
-		public void reduce(Text key, Iterator<IntWritable> values,
-				OutputCollector<Text, IntWritable> out, Reporter reporter)
-				throws IOException {
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
 			int sum = 0;
-			while (values.hasNext()) {
-				sum += values.next().get();
+			for (IntWritable value : values) {
+				sum += value.get();
 			}
-			out.collect(key, new IntWritable(sum));
-			reporter.incrCounter("wordcount", "reduce", 1);
-		}	
+			context.write(key, new IntWritable(sum));
+		}
 	}
 	
 	@Override
@@ -55,25 +58,27 @@ public class WordCount extends Configured implements Tool {
 	        return -1;
 	      }
 	    
-	    JobConf conf = new JobConf(getConf(), WordCount.class);
-	    conf.setJobName("wordcount");
-		conf.setJarByClass(WordCount.class);
+	    Job job = new Job(getConf());
+	    job.setJarByClass(WordCount.class);
+	    
+		job.setInputFormatClass(TextInputFormat.class);
 		
-		conf.setOutputKeyClass(Text.class);
-		conf.setOutputValueClass(IntWritable.class);
+		job.setMapperClass(Map.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
 		
-		conf.setMapperClass(Map.class);
-		conf.setCombinerClass(Reduce.class);
-		conf.setReducerClass(Reduce.class);
+		job.setReducerClass(Reduce.class);
+		job.setCombinerClass(Reduce.class);
 		
-		conf.setInputFormat(TextInputFormat.class);
-		conf.setOutputFormat(TextOutputFormat.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
 		
-		FileInputFormat.setInputPaths(conf, new Path(args[0]));
-		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+		job.setOutputFormatClass(TextOutputFormat.class);
+		
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-	    JobClient.runJob(conf);
-	    return 0;
+		return job.waitForCompletion(true) ? 0 :  1;
 	}
 	    	
 	public static void main(String[] args) throws Exception {
