e9917fd40944f478a8ccd828a4d17f89517df3db
Andrew Whitaker <whitaker@cs.washington.edu>
2013-07-18 14:36:24 -0700

    convert word count to tool interface

--- src/edu/washington/escience/WordCount.java
+++ src/edu/washington/escience/WordCount.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.StringTokenizer;
 
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-public class WordCount {
+public class WordCount extends Configured implements Tool {
 
 	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
 
@@ -40,13 +43,21 @@ public class WordCount {
 			}
 			out.collect(key, new IntWritable(sum));
 			reporter.incrCounter("wordcount", "reduce", 1);
-		}
-		
+		}	
 	}
 	
-	public static void main(String[] args) throws Exception {
-		JobConf conf = new JobConf(WordCount.class);
-		conf.setJobName("wordcount");
+	@Override
+	public int run(String[] args) throws Exception {
+	    if (args.length != 2) {
+	        System.err.printf("Usage: %s [generic options] <input> <output>\n",
+	            getClass().getSimpleName());
+	        ToolRunner.printGenericCommandUsage(System.err);
+	        return -1;
+	      }
+	    
+	    JobConf conf = new JobConf(getConf(), WordCount.class);
+	    conf.setJobName("wordcount");
+		conf.setJarByClass(WordCount.class);
 		
 		conf.setOutputKeyClass(Text.class);
 		conf.setOutputValueClass(IntWritable.class);
@@ -60,7 +71,13 @@ public class WordCount {
 		
 		FileInputFormat.setInputPaths(conf, new Path(args[0]));
 		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
-		
-		JobClient.runJob(conf);
+
+	    JobClient.runJob(conf);
+	    return 0;
+	}
+	    	
+	public static void main(String[] args) throws Exception {
+		int exitCode = ToolRunner.run(new WordCount(), args);
+		System.exit(exitCode);
 	}
 }
