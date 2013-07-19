4af6eb2319a59618b9bbff10b228e79375605572
Andrew Whitaker <whitaker@cs.washington.edu>
2013-07-19 10:00:59 -0700

    MR program for emitting vertices in warc files

---
+++ src/edu/washington/escience/warc/WarcVertices.java
@@ -0,0 +1,89 @@
+package edu.washington.escience.warc;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.StringTokenizer;
+import java.util.zip.GZIPInputStream;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.jwat.warc.WarcReader;
+import org.jwat.warc.WarcReaderFactory;
+import org.jwat.warc.WarcRecord;
+
+import edu.washington.escience.util.WholeFileInputFormat;
+
+public class WarcVertices extends Configured implements Tool {
+
+	public static class Map extends Mapper<NullWritable, BytesWritable, Text, IntWritable > {
+
+		private static final IntWritable one = new IntWritable(1);
+		
+		@Override
+		public void map(NullWritable key, BytesWritable value, Context context) 
+				throws IOException, InterruptedException {
+			ByteArrayInputStream bis = new ByteArrayInputStream(value.getBytes());
+			Text uriText = new Text();
+
+			// Work around broken gzip decoder in jwat
+			InputStream in = new GZIPInputStream(bis);
+			
+			WarcReader reader = WarcReaderFactory.getReaderUncompressed(in);
+			WarcRecord record;
+			while ( (record = reader.getNextRecord()) != null ) {
+				String targetUri = record.header.warcTargetUriStr;
+		        	
+				if (targetUri == null)
+					continue;
+				uriText.set(targetUri);
+				context.write(uriText, one);
+			}
+		}
+	}
+	
+	@Override
+	public int run(String[] args) throws Exception {
+	    if (args.length != 2) {
+	        System.err.printf("Usage: %s [generic options] <input> <output>\n",
+	            getClass().getSimpleName());
+	        ToolRunner.printGenericCommandUsage(System.err);
+	        return -1;
+	      }
+	    
+	    Job job = new Job(getConf());
+	    job.setJarByClass(WarcVertices.class);
+	    
+		job.setInputFormatClass(WholeFileInputFormat.class);
+		
+		job.setMapperClass(Map.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		job.setOutputFormatClass(TextOutputFormat.class);
+		
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		// Nothing to reduce; this ensures that our mapper's output goes to HDFS
+		job.setNumReduceTasks(0);
+		return job.waitForCompletion(true) ? 0 :  1;
+	}
+	    	
+	public static void main(String[] args) throws Exception {
+		int exitCode = ToolRunner.run(new WarcVertices(), args);
+		System.exit(exitCode);
+	}
+}
