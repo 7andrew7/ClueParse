e4c466f7451d31b2d262b7ebadec6cf4ecaf1a7d
Andrew Whitaker <whitaker@cs.washington.edu>
2013-07-22 07:59:45 -0700

    Add noop MR job

---
+++ src/edu/washington/escience/warc/Noop.java
@@ -0,0 +1,68 @@
+package edu.washington.escience.warc;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import edu.washington.escience.util.WholeFileInputFormat;
+
+/**
+ * No-op Map/Reduce job that scans every input file but produces no output.
+ */
+public class Noop extends Configured implements Tool {
+
+	public static class Map extends Mapper<NullWritable, BytesWritable, Text, Text > {
+		
+		@Override
+		public void map(NullWritable key, BytesWritable value, Context context) 
+				throws IOException, InterruptedException {
+
+		}
+	}
+	
+	@Override
+	public int run(String[] args) throws Exception {
+	    if (args.length != 2) {
+	        System.err.printf("Usage: %s [generic options] <input> <output>\n",
+	            getClass().getSimpleName());
+	        ToolRunner.printGenericCommandUsage(System.err);
+	        return -1;
+	      }
+	    
+	    Job job = new Job(getConf());
+	    job.setJarByClass(Noop.class);
+	    
+		job.setInputFormatClass(WholeFileInputFormat.class);
+		
+		job.setMapperClass(Map.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		
+		job.setNumReduceTasks(0);
+
+		job.setOutputFormatClass(TextOutputFormat.class);
+	
+		System.out.println("input path: " + args[0]);
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		return job.waitForCompletion(true) ? 0 :  1;
+	}
+	    	
+	public static void main(String[] args) throws Exception {
+		int exitCode = ToolRunner.run(new Noop(), args);
+		System.exit(exitCode);
+	}
+}
